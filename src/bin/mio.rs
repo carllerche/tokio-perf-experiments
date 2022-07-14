b49550048da6ad90f87881e755fda90677d2de13
Carl Lerche <me@carllerche.com>
2022-07-14 15:06:16 -0700

    some tweaks and use send instead of write

--- src/bin/mio.rs
+++ src/bin/mio.rs
@@ -1,8 +1,11 @@
 use mio::net::{TcpListener, TcpStream};
 use mio::Interest;
 use mio::{Events, Poll, Token};
-use std::collections::HashMap;
 use std::io::{Read, Write};
+
+use std::os::unix::io::AsRawFd;
+
+const RESP: &[u8] = b"HTTP/1.1 200 OK\r\nContent-length: 12\r\n\r\nHello world\n";
 
 fn main() {
     let addr = "127.0.0.1:9000".parse().unwrap();
@@ -10,59 +13,64 @@ fn main() {
 
     let mut poll = Poll::new().unwrap();
     poll.registry()
-        .register(&mut listener, Token(0), Interest::READABLE)
+        .register(&mut listener, Token(1024), Interest::READABLE)
         .unwrap();
 
-    let mut counter: usize = 0;
-    let mut sockets: HashMap<Token, TcpStream> = HashMap::new();
-    let mut buffer = [0 as u8; 1024];
-
-    let response = b"HTTP/1.1 200 OK\r\nContent-length: 12\r\n\r\nHello world\n";
+    let mut sockets = slab::Slab::with_capacity(1024);
+    let mut buffer = vec![0 as u8; 1024 * 128];
 
     let mut events = Events::with_capacity(1024);
     loop {
         poll.poll(&mut events, None).unwrap();
         for event in &events {
             match event.token() {
-                Token(0) => loop {
+                Token(1024) => loop {
                     match listener.accept() {
                         Ok((mut socket, _)) => {
-                            counter += 1;
-                            let token = Token(counter);
+
+                            socket.set_nodelay(true).unwrap();
+
+                            let entry = sockets.vacant_entry();
+                            let token = Token(entry.key());
 
                             poll.registry()
                                 .register(&mut socket, token, Interest::READABLE)
                                 .unwrap();
-                            sockets.insert(token, socket);
+
+                            entry.insert(socket);
                         }
                         Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
                         Err(_) => break,
                     }
                 },
                 token if event.is_readable() => {
-                    let socket = sockets.get_mut(&token).unwrap();
-                    let read = socket.read(&mut buffer);
-                    match read {
-                        Ok(0) => {
-                            sockets.remove(&token);
-                            continue;
-                        }
-                        Ok(_n) => {}
-                        Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                            println!("socket  read WouldBlock");
-                            break;
-                        }
-                        Err(_) => break,
+                    if process(&mut sockets[token.0], &mut buffer[..]) {
+                        sockets.remove(token.0);
                     }
-
-                    let n = socket.write(response).unwrap();
-                    assert_eq!(n, response.len());
-                }
-                _token if event.is_writable() => {
-                    unreachable!();
                 }
                 _ => unreachable!(),
             }
         }
     }
+}
+
+fn process(socket: &mut TcpStream, buffer: &mut [u8]) -> bool {
+    let read = socket.read(&mut buffer[..]);
+    match read {
+        Ok(0) => {
+            return true;
+        }
+        Ok(_) => {}
+        Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+            panic!();
+        }
+        Err(_) => {
+            return true;
+        }
+    }
+
+    let n = unsafe { libc::send(socket.as_raw_fd(), RESP.as_ptr() as _, RESP.len(), libc::MSG_NOSIGNAL) } as usize;
+
+    assert_eq!(n, RESP.len());
+    false
 }
